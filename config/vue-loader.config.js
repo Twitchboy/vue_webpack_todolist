6824b4e7087f654a8e8d419cfe3a433bf25c017d
Junting <342766475@qq.com>
2018-06-08 08:08:26 +0800

    新增 notes 分支，实验 Vue 的一些方法功能的 demo 集合

--- config/vue-loader.config.js
+++ config/vue-loader.config.js
@@ -1,18 +1,18 @@
 // const docsLoader = require('docsLoader');
 
-module.exports = (_mode) => {
-    // vue-loader options 配置参数
-    return {
-        preserveWhitespace: true,   // 过滤空格开启
-        extractCSS: !_mode,         // 将 vue 里的 style 提取出来
-        cssModules: {},             //
-        // hotReload: false     // 热重载，根据环境变量自动生成不需要配置
-        // loader: {
-            // 'docs': docsLoader,  // 自定义 Loader, 给各个组件 写文档用的
-            //  js:
-            //  css:
-        // },
-        // preLoader: {},           // 加载loader之前先加载
-        // postLoader: {}           // 加载loader之后再加载
-    }
-}
+module.exports = _mode => {
+  // vue-loader options 配置参数
+  return {
+    preserveWhitespace: true, // 过滤空格开启
+    extractCSS: !_mode, // 将 vue 里的 style 提取出来
+    cssModules: {} //
+    // hotReload: false     // 热重载，根据环境变量自动生成不需要配置
+    // loader: {
+    // 'docs': docsLoader,  // 自定义 Loader, 给各个组件 写文档用的
+    //  js:
+    //  css:
+    // },
+    // preLoader: {},           // 加载loader之前先加载
+    // postLoader: {}           // 加载loader之后再加载
+  };
+};
